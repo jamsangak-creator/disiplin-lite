41be79c3e0cf1cdb3f44c3b4eb1239452fb1699d
SMK SIKUATI II <jamsangak@gmail.com>
2025-08-16 15:24:55 +0800

    Update config.js with Google Script endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-let ENDPOINT = ""; const EMAIL_NOTIFY = true; const EMAIL_THRESHOLD = 3;
+let ENDPOINT = "https://script.google.com/macros/s/AKfycbyobRsQi2ah2wYDdErlH4rC48h3G8oStsCV7yGAKNIUaIOdpm9ZM2vgQzET1-Z19YneIw/exec"; const EMAIL_NOTIFY = true; const EMAIL_THRESHOLD = 3;
