a3cc720d04106b57a280dbbf426ffaf14cdd93f3
SMK SIKUATI II <jamsangak@gmail.com>
2025-08-19 20:17:30 +0800

    Delete config.js

--- config.js
+++
@@ -1,1 +0,0 @@
-let ENDPOINT = "https://script.google.com/macros/s/AKfycbyobRsQi2ah2wYDdErlH4rC48h3G8oStsCV7yGAKNIUaIOdpm9ZM2vgQzET1-Z19YneIw/exec"; const EMAIL_NOTIFY = true; const EMAIL_THRESHOLD = 3;
