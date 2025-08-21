a8a452b6735846e9d8ce8a20fc5b878875e72448
SMK SIKUATI II <jamsangak@gmail.com>
2025-08-21 23:19:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // ====== KONFIG ======
-const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbxymXS8iJaAyGrlnhXPkCQOCIdd-EkAAo_ZXOoCeY2qQZcRbXBScfZy_nSq9H2Li1bcxg/exec";
 
 // ====== UTIL ======
 const $ = (sel) => document.querySelector(sel);
